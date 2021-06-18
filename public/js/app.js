4c849a9986f30a5f173b1230205cf142af78369f
UntillNIGHT <lengleng9090@gmail.com>
2021-06-18 15:35:10 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,3 @@
-console.log('Client side javascript is loaded!');
-
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data);
-    })
-})
-
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error);
-        }else
-        console.log(data);
-    })
-})
-
 const weatherForm = document.querySelector('form');
 const search =document.querySelector('input')
 const message1 = document.querySelector('#message-1');
@@ -24,7 +7,7 @@ weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     message1.textContent = 'Loading...';
     message2.textContent = '';
-    fetch('http://localhost:3000/weather?address='+search.value).then((response)=>{
+    fetch('/weather?address='+search.value).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             message1.textContent = data.error;
